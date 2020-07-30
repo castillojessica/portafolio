d68f70d8b3ce929956cfafae536177d169ced048
castillojessica <jessica.castillo95@hotmail.com>
2020-07-29 20:08:36 -0500

    ok

--- src/components/assets/data/Data.js
+++ src/components/assets/data/Data.js
@@ -10,6 +10,13 @@ let portafolio=[
         texto:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
         imagen:'https://www.maxcf.es/wp-content/uploads/2017/05/simplemag.jpg',
         link:"https://google.com"
+    },
+    {
+        id:3,
+        nombreProyecto:"Red social",
+        texto:"Una red socual",
+        imagen:'https://www.maxcf.es/wp-content/uploads/2017/05/simplemag.jpg',
+        link:"https://google.com"
     }
 ]
 export default portafolio;
